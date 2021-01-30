9dd6215d47573e061605b3c209fcc22fb397989d
dchernyavsky <dchernyavsky678@gmail.com>
2021-01-30 23:19:23 +0300

    task 3

--- src/lessons/lesson3/API.ts
+++ src/lessons/lesson3/API.ts
@@ -3,13 +3,15 @@ import axios from 'axios';
 const configOMB = {
     baseURL: 'http://www.omdbapi.com',
 };
-const key = '';
+const key = '?apikey=b16cb7f4';
 const axiosInstance = axios.create(configOMB);
 
 const API = {
     searchFilmsByTitle: (title: string) => {
+        return axiosInstance.get(`${key}&s=${title}`)
     },
     searchFilmsByType: (title: string, type: string) => {
+        return axiosInstance.get(`${key}&t=${title}&type=${type}`)
     }
 };
 
